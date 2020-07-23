23fd454f038b68dcc0bbd8d6021730ea1ef694fb
NaCN2020 <67950956+NaCN2020@users.noreply.github.com>
2020-07-24 01:11:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
+import { HashRouter as Router,Route} from 'react-router-dom';
 import FileField from './FileField';
 import Showfiles from './showfiles';
 import Showcontent from './showcontent';
